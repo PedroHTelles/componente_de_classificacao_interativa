a1b0663e1fd4b2efa8e57da0042c2f1c97f33bdc
Pedro Henrique Florêncio Telles <phftelles@gmail.com>
2022-10-21 15:07:32 -0300

    Diminuindo código JS com nó de repetição

--- js/index.js
+++ js/index.js
@@ -1,25 +1,19 @@
-var form = document.querySelector('#classification')
-var rating = document.getElementsByName('rating')
-var component_1 = document.querySelector('#component-1')
-var component_2 = document.querySelector('#component-2')
-var res = document.querySelector('#res')
-
+const form = document.querySelector('#classification')
+const rating = document.getElementsByName('rating')
+const component_1 = document.querySelector('#component-1')
+const component_2 = document.querySelector('#component-2')
+const res = document.querySelector('#res')
+// Retornando o valor "checked".
 function check(rating) {
-    var val = ''
-    if (rating[0].checked) {
-        val = rating[0].value
-    } else if (rating[1].checked) {
-        val = rating[1].value
-    } else if (rating[2].checked) {
-        val = rating[2].value
-    } else if (rating[3].checked) {
-        val = rating[3].value
-    } else if (rating[4].checked) {
-        val = rating[4].value
-    }
+    let val = ``
+    rating.forEach((obj,index)=>{
+        if (rating[index].checked == true) {
+            val = rating[index].value          
+        }       
+    })
     return val
 }
-
+// Ação-pós "subimit".
 function sub(event) {
     event.preventDefault()
 
